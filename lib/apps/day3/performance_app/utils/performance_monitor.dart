2046d07d5502af1958e528ae66aace400ef48315
Saqib Hanif <muhammad.saqib@doozielabs.com>
2026-03-31 20:38:10 +0500

    Day 3: App 2 - initial

---
+++ lib/apps/day3/performance_app/utils/performance_monitor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+import 'dart:developer' as developer;
+
+class PerformanceMonitor {
+  static final Map<String, int> _buildCounts = {};
+  static final Map<String, DateTime> _buildTimestamps = {};
+
+  
+}
